af77dcb7f89179ac2f4c8d7fcf711267ce903c88
Dillon Duncan <dillonduncan0607@gmail.com>
2023-11-05 16:21:46 -0500

    Vista pantalla juegoPlay

---
+++ app/src/main/java/com/example/millonariofirebase/JuegoPlay_Activity.kt
@@ -0,0 +1,11 @@
+package com.example.millonariofirebase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class JuegoPlay_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_juego_play)
+    }
+}
